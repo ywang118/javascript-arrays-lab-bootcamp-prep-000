d00ee69bb2ed616ae476dbd320ac0e5a238ee592
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-03-25 21:37:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,16 +29,10 @@ function appendKitten(name) {
   kittens_new = [...kittens, name]
   return kittens_new;
 }
-escribe('appendKitten(name)', function() {
-    it('appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      expect(appendKitten("Broom")).toEqual(["Milo", "Otis", "Garfield", "Broom"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
 
 
 function prependKitten(name) {
-  kittens_new = kittens.slice(0,kittens.length - 1 )
+  kittens_new = kittens.slice(1,kittens.length )
   return kittens_new;
 }
+
